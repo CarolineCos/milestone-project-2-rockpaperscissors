bb330198894b90251b9305e5ea8f67d341c06ac0
CarolineCos <carleycos@gmail.com>
2021-10-21 17:26:23 +0000

    Get basic javaScript by targeting elements to be manipulated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
 let buttons = document.getElementsByClassName("controls");
-let 
+let playerImage = document.getElementById("player-image");
+let computerImage = document.getElementById("computer-image");
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("compute-score");
+let choices = ["rock", "paper", "scissors"];
+
