ff895dd41be0447c13f01f480cd08aa57ca44156
CarolineCos <carleycos@gmail.com>
2021-10-23 18:29:43 +0000

    remove check answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ const computerImage = document.getElementById("computer-image");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 let choices = ["rock", "paper", "scissors"];
+let playerChoice 
+let computerChoice
 
 /** add event listener to buttons */
 
@@ -11,56 +13,42 @@ for(let button of buttons){
     button.addEventListener("click", function(){
         let playerChoice = this.getAttribute("data-choice");
         play(playerChoice);
-    }
-    )}
+     } 
+)
+
+    checkWinner();
+}
 
 function play(playerChoice) {
     playerImage.src = `assets/images/${choices[playerChoice]}.jpg`;
     playerImage.alt = choices[playerChoice];
 
-    let computerChoice = Math.floor(Math.random() * 3);
+    computerChoice = Math.floor(Math.random() * 3);
     computerImage.src =`assets/images/${choices[computerChoice]}.jpg`;
     computerImage.alt = choices[computerChoice];
-    
+   
 }
 
-/** check winner */
 
 
 function checkWinner(){
-   if (computerChoice === playerChoice){
-    alert("Its a draw");
-   }
+    
+        }
 
-   if(computerChoice === "rock" && playerChoice === "paper"){
-       alert("You Win");
-       incrementScore(playerScore);
-   }
 
-   if(computerChoice === "rock" && playerChoice === "scissors"){
-    alert("Computer Wins");
-    incrementScore(computerScore);
-}
 
-    if(computerChoice === "paper" && playerChoice === "rock"){
-    alert("Computer Wins");
-    incrementScore(computerScore);
-}
-    if(computerChoice === "paper" && playerChoice === "scissors"){
-    alert("You Win");
-    incrementScore(playerScore);
-}
-    if(computerChoice === "scissors" && playerChoice === "paper"){
-    alert("Computer Wins");
-    incrementScore(computerScore);
-}
-    if(computerChoice === "scissors" && playerChoice === "rock"){
-    alert("You Win");
-    incrementScore(playerScore);
-}
-}
-
-checkWinner();
+    
 /** increment score */
+
+function incrementPlayerScore(){
+    let oldScore = playerScore.innerHTML;
+    playerScore.innerHTML = ++oldScore;
+}
+
+function incrementComputerScore(){
+    let oldScore = computerScore.innerHTML;
+    computerScore.innerHTML = ++oldScore;
+}
+
 
 /** possibly add winner message */
