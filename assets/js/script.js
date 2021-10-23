39977aa8171ad97816e4f79d7a0c3a0d16d747fc
CarolineCos <carleycos@gmail.com>
2021-10-23 12:01:37 +0000

    Add js to buttons and pull image

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,19 @@ let choices = ["rock", "paper", "scissors"];
 /** add event listener to buttons */
 
 for(let button of buttons){
-    button.addEventListener("click", function){
-        let playerChoice = this.getAttribute ("data-choice");
+    button.addEventListener("click", function(){
+        let playerChoice = this.getAttribute("data-choice");
         play(playerChoice);
     }
-}
+    )}
 
 function play(playerChoice) {
     playerImage.src = `assets/images/${choices[playerChoice]}.jpg`;
     playerImage.alt = choices[playerChoice];
 
-    let computerChoice = Math.floor(Math.random() * choices.length);
-    computerChoice.src =`assets/images/${choices[computerChoice]}.jpg`;
-    computerChoice.alt = choices[computerChoice];
+    let computerChoice = Math.floor(Math.random() * 3);
+    computerImage.src =`assets/images/${choices[computerChoice]}.jpg`;
+    computerImage.alt = choices[computerChoice];
     
 }
+
