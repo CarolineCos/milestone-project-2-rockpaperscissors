bf8f02de8887a842dd96aa748004fe8677b58df3
CarolineCos <carleycos@gmail.com>
2021-10-24 17:23:59 +0000

    Fix ttempt of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,52 +26,56 @@ function play(playerChoice) {
 
     updateScores(result);
 }
+
 /**
  * rock === 0
  * paper === 1
  * scissors == 2
  */
 
-   function checkWinner(choices[computer], choices[playerChoice]) {
+   function checkWinner(choices[computerChoice], choices[playerChoice]) {
 
-    if (choices[computer] === choices[playerChoice]){
-        alert("its a tie");
-        // rock vs paper
-    } else if (choices[computer]===0 && choices[playerChoice] === 1){
+    if (choices[computerChoice] === choices[playerChoice]){
+        alert("It's a tie");
+    } 
+     // rock vs paper
+    else if (choices[computerChoice]===0 && choices[playerChoice] === 1){
         alert("You win");
         incrementPlayerScore();
-        //rock vs scissors
-    } else if (choices[computer]===0 && choices[playerChoice] === 2){
+    }
+    //rock vs scissors 
+    else if (choices[computerChoice]===0 && choices[playerChoice] === 2){
         alert("You lose");
         incrementComputerScore();
-        //paper vs scissors
-    } else if (choices[computer]===1 && choices[playerChoice] === 2){
+    } 
+    //paper vs scissors
+    else if (choices[computerChoice]===1 && choices[playerChoice] === 2){
         alert("You win");
         incrementPlayerScore();
         // scissors vs paper
-    } else if (choices[computer]===2 && choices[playerChoice] === 1){
+    } else if (choices[computerChoice]===2 && choices[playerChoice] === 1){
         alert("You lose");
         incrementComputerScore();
         // scissors vs rock
-    } else if (choices[computer]===2 && choices[playerChoice] === 0){
+    } else if (choices[computerChoice]===2 && choices[playerChoice] === 0){
         alert("You win");
         incrementPlayerScore();
-        //scissors vs paper
-    } else if (choices[computer]===2 && choices[playerChoice] === 1){
-        alert("You lose");
-        incrementComputerScore();
-    }
+
+     }
    }
 
    function updateScores() {
 
     function incrementPlayerScore() {
-      playerScore.innerHTML= parseInt(playerScore.innerHTML)+1;
+      let oldScore =  parseInt(playerScore.innerText);
+      playerScore.innerText= ++oldScore;
+      
     }
     
     function incrementComputerScore() {
-        computerScore.innerHTML = parseInt(computerScore.innerHTML)+1;
-               
+        let oldScore =  parseInt(computerScore.innerText);
+        computerScore.innerText= ++oldScore;                       
      }
    }
+    
 /** possibly add winner message */
